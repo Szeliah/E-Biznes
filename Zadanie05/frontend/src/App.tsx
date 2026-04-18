f0a0a3aff744282451f70454604742b0f3c3ac57
Szeliah <szeliszymon@gmail.com>
2026-04-18 15:51:05 +0200

    Initial commit

---
+++ Zadanie05/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Welcome to Zadanie05</h1>
+    </>
+  )
+}
+
+export default App
