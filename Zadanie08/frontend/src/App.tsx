038701420b30aad5e7e22a6b7d7bc07c8e241e78
Szeliah <szeliszymon@gmail.com>
2026-05-25 14:20:51 +0200

    chors: before task 3.0

--- Zadanie08/frontend/src/App.tsx
+++ Zadanie08/frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-function App() {
+import Header from "./components/Header.tsx"
+import Signin from "./components/Signin.tsx"
+import Register from "./components/Register.tsx"
+
+import "./App.css"
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] =
+  useState(false);
 
   return (
-    <>
-    
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Header isLoggedIn={isLoggedIn}/>} />
+        <Route path="/signin" element={<Signin setIsLoggedIn={setIsLoggedIn}/>} />
+        <Route path="/register" element={<Register setIsLoggedIn={setIsLoggedIn}/>}/>
+      </Routes>
+  );
 }
 
-export default App
+
